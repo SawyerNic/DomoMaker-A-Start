4e9a71da4e506820482f13934f55411a4974cb71
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-04-01 16:36:40 -0400

    update

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,46 +1,44 @@
 const models = require('../models');
-const Domo = models.Domo;
+
+const { Domo } = models;
 
 const makerPage = async (req, res) => {
-    try{
-        const query = {owner: req.session.account._id};
-        const docs = await Domo.find(query).select('name age').lean().exec();
+  try {
+    const query = { owner: req.session.account._id };
+    const docs = await Domo.find(query).select('name age').lean().exec();
 
-        return res.render('app', {domos: docs});
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: 'An error occurred' });
-    }
-}
-
-const makeDomo = async (req, res) => {
-    if (!req.body.name || !req.body.age) {
-        return res.status(400).json({ error: 'RAWR! Name and age are required' });
-    }
-
-    const domoData = {
-        name: req.body.name,
-        age: req.body.age,
-        owner: req.session.account._id,
-    };
-
-    try{
-        const newDomo = new Domo(domoData);
-        await newDomo.save();
-        return res.json({ redirect: '/maker' });
-    } catch (err) {
-        console.log(err);
-        if (err.code === 11000) {
-            return res.status(400).json({ error: 'Domo already exists.' });
-        }
-        return res.status(500).json({ error: 'An error occurred making domo!' });
-    }
-}
-
-
-
-module.exports = {
-    makerPage,
-    makeDomo,
+    return res.render('app', { domos: docs });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'An error occurred' });
+  }
 };
 
+const makeDomo = async (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'RAWR! Name and age are required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.json({ redirect: '/maker' });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists.' });
+    }
+    return res.status(500).json({ error: 'An error occurred making domo!' });
+  }
+};
+
+module.exports = {
+  makerPage,
+  makeDomo,
+};
